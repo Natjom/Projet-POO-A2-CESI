ba68e113cd9f70b1be8b9ff993e0b3e33131073c
Natjom <n4t10m@gmail.com>
2024-11-21 11:46:43 +0100

    v2.2111.1b

--- src/map.cpp
+++ src/map.cpp
@@ -8,13 +8,13 @@ Map::Map(int cellSize, int speed)
     : cellSize(cellSize), speed(speed)
 {
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    int screenWidth = desktop.width - 10;         // 80% de la largeur de l'écran
-    int screenHeight = desktop.height - 200; // 80% de la hauteur moins la barre inférieure
+    int screenWidth = desktop.width - 10;       
+    int screenHeight = desktop.height - 200; 
     gridWidth = screenWidth / cellSize;
     gridHeight = screenHeight / cellSize;
     grid = vector<vector<Cell>>(gridWidth, vector<Cell>(gridHeight));
     window.create(sf::VideoMode(screenWidth, screenHeight + 100), "Game of Life");
-    window.setPosition(sf::Vector2i(0, 0)); // Place la fenêtre à 50 pixels des bords gauche et supérieur de l'écran
+    window.setPosition(sf::Vector2i(0, 0));
 }
 
 void Map::initializeGrid()
@@ -44,9 +44,9 @@ void Map::handleMouseClick(sf::Vector2i mousePosition)
     float buttonHeight = 40;
     float margin = 20;
     float startX = 10;
-    float startY = gridHeight * cellSize + 30; // Position verticale des boutons
+    float startY = gridHeight * cellSize + 30; 
 
-    for (int i = 0; i < 3; ++i) // Vérifier les trois boutons
+    for (int i = 0; i < 3; ++i)
     {
         float buttonX = startX + i * (buttonWidth + margin);
         float buttonY = startY;
@@ -55,44 +55,35 @@ void Map::handleMouseClick(sf::Vector2i mousePosition)
             mousePosition.y >= buttonY && mousePosition.y <= buttonY + buttonHeight)
         {
             cout << "Button " << i + 1 << " clicked!" << endl;
-            // Effectuez l'action du bouton ici
         }
     }
 }
 
 void Map::renderSidebar()
 {
-    // Définir la hauteur de la barre inférieure
     float sidebarHeight = 100.0f;
     float sidebarWidth = gridWidth * cellSize;
-
-    // Dessiner la barre inférieure
     sf::RectangleShape sidebar(sf::Vector2f(sidebarWidth, sidebarHeight));
-    sidebar.setFillColor(sf::Color(50, 50, 50));   // Couleur gris foncé
-    sidebar.setPosition(0, gridHeight * cellSize); // Placée en bas de la grille
+    sidebar.setFillColor(sf::Color(50, 50, 50));   
+    sidebar.setPosition(0, gridHeight * cellSize); 
     window.draw(sidebar);
-
-    // Dessiner des boutons
     float buttonWidth = 120;
     float buttonHeight = 40;
-    float margin = 20;                                                         // Espace entre les boutons
-    float startX = 10;                                                         // Position x initiale
-    float startY = gridHeight * cellSize + (sidebarHeight - buttonHeight) / 2; // Centrer verticalement
-
-    for (int i = 0; i < 3; ++i) // Trois boutons
+    float margin = 20;                                                         
+    float startX = 10;                                                         
+    float startY = gridHeight * cellSize + (sidebarHeight - buttonHeight) / 2; 
+    for (int i = 0; i < 3; ++i) 
     {
         sf::RectangleShape button(sf::Vector2f(buttonWidth, buttonHeight));
-        button.setFillColor(sf::Color(100, 100, 100)); // Couleur gris clair
+        button.setFillColor(sf::Color(100, 100, 100)); 
         button.setPosition(startX + i * (buttonWidth + margin), startY);
         window.draw(button);
-
-        // Ajouter du texte (optionnel)
         sf::Font font;
-        if (font.loadFromFile("../fonts/arial.ttf")) // Assurez-vous que "arial.ttf" existe
+        if (font.loadFromFile("../fonts/arial.ttf"))
         {
             sf::Text buttonText;
             buttonText.setFont(font);
-            buttonText.setString("Button " + std::to_string(i + 1)); // Nom des boutons
+            buttonText.setString("Button " + std::to_string(i + 1));
             buttonText.setCharacterSize(16);
             buttonText.setFillColor(sf::Color::White);
             buttonText.setPosition(startX + i * (buttonWidth + margin) + 10, startY + 10);
@@ -130,7 +121,6 @@ void Map::rule(int x, int y)
 
 void Map::renderGrid()
 {
-    // Applique les règles de la grille
     for (int x = 0; x < gridWidth; ++x)
     {
         for (int y = 0; y < gridHeight; ++y)
@@ -145,8 +135,6 @@ void Map::renderGrid()
             grid[x][y].update();
         }
     }
-
-    // Dessine la grille
     window.clear();
     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
     for (int x = 0; x < gridWidth; ++x)
@@ -165,10 +153,7 @@ void Map::renderGrid()
             window.draw(cell);
         }
     }
-
-    // Dessine la barre latérale avec les boutons
     renderSidebar();
-
     window.display();
 }
 
